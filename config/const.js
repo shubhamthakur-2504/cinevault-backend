78d48c0f18ec6621f81c4e96c92bbb80cab2fc1b
shubhamthakur-2504 <shubhamthakur60278@gmail.com>
2025-12-09 20:27:32 +0530

    feat: add user model

--- config/const.js
+++ config/const.js
@@ -5,5 +5,10 @@ const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
 const PORT = process.env.PORT || 3000;
 const DB_NAME = process.env.DB_NAME || 'mydatabase';
 const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/';
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
+const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
+const JWT_REFRESH_EXPIRY = process.env.JWT_REFRESH_EXPIRY;
+const JWT_ACCESS_EXPIRY = process.env.JWT_ACCESS_EXPIRY;
+const HTTP_COOKIE_EXPIRES_IN = 7 * 24 * 60 * 60 * 1000; 
 
 export { CLIENT_URL, PORT, DB_NAME, MONGODB_URL };
---
+++ models/user.model.js
@@ -0,0 +1,84 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { JWT_REFRESH_SECRET, JWT_ACCESS_SECRET, JWT_REFRESH_EXPIRY, JWT_ACCESS_EXPIRY } from "../config/const.js";
+
+const userSchema = new Schema({
+    userName: { 
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    passwordHash: {
+        type: String,
+        required: true,
+        select: false
+    },
+    role: {
+        type: String,
+        enum: ['USER', 'ADMIN'],
+        default: 'USER'
+    },
+    isActive: {
+        type: Boolean,
+        default: true 
+    },
+    refreshToken: {
+        type: String,
+        default: null,
+        select: false
+    }
+}, { timestamps: true })
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('passwordHash')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.passwordHash = await bcrypt.hash(this.passwordHash, salt);
+    next();
+});
+
+userSchema.methods.comparePassword = async function(password) {
+    return bcrypt.compare(password, this.passwordHash);
+};
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        { userId: this._id, userName: this.userName },
+        JWT_ACCESS_SECRET,
+        { expiresIn: JWT_ACCESS_EXPIRY }
+    );
+};
+
+userSchema.methods.generateRefreshToken = async function() {
+    const refreshToken = jwt.sign(
+        { userId: this._id, userName: this.userName },
+        JWT_REFRESH_SECRET,
+        { expiresIn: JWT_REFRESH_EXPIRY }
+    );
+    this.refreshToken = refreshToken;
+    await this.save();
+    return refreshToken;
+};
+
+userSchema.methods.revokeRefreshToken = async function() {
+  this.refreshToken = null;
+  await this.save();
+};
+
+userSchema.set('toJSON', {
+  transform: function(doc, ret, options) {
+    delete ret.passwordHash;
+    delete ret.refreshToken;
+    delete ret.__v;
+    return ret;
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User
