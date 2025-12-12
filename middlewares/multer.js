7d35bd3617509f3109c314b5a56f6706fb51ea7b
shubhamthakur-2504 <shubhamthakur60278@gmail.com>
2025-12-13 00:32:27 +0530

    feat: add multer middleware for file uploads

---
+++ middlewares/multer.js
@@ -0,0 +1,18 @@
+import multer from "multer";
+import {v4 as uuidv4} from "uuid";
+import path from "path";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, "./public/temps");
+    },
+    filename: function (req, file, cb){
+        const extName = path.extname(file.originalname);
+        const fileName =`${uuidv4()}${extName}`
+        cb(null, fileName)
+    }
+})
+
+const upload = multer({storage:storage})
+
+export default upload
